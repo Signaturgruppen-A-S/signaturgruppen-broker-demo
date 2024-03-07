42a7e71d8472e73057fb0f350e1be69dd30c797d
Tue Skeltved <bigheadedmonster@users.noreply.github.com>
2024-03-07 13:51:49 +0100

    Logout via api (#5)
    * Refactoring, removing NemID, simplifying.
    
    * popup size and signing flow working
    
    * Logout using API, not redirect

--- NetsBrokerIntegration.NetCore/Controllers/HomeController.cs
+++ NetsBrokerIntegration.NetCore/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -8,6 +10,12 @@ namespace NetsBrokerIntegration.NetCore.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
         public IActionResult Index()
         {
             return View();
@@ -27,13 +35,24 @@ namespace NetsBrokerIntegration.NetCore.Controllers
 
         public async Task<IActionResult> Logout()
         {
+            await ApiLogoutFromNetseidBroker();
+
             if (User?.Identity.IsAuthenticated == true)
             {
                 await HttpContext.SignOutAsync();
-                return SignOut(new AuthenticationProperties { RedirectUri = "/Home/Index" }, "oidc");
             }
 
             return RedirectToAction("Index", "Home");
+        }
+
+        private async Task ApiLogoutFromNetseidBroker()
+        {
+            var httpClient = httpClientFactory.CreateClient();
+            var idToken = await HttpContext.GetTokenAsync("id_token");
+            if(idToken != null)
+            {
+                await httpClient.PostAsJsonAsync("https://pp.netseidbroker.dk/op/api/v1/session/logout", new { id_token = idToken });
+            }
         }
     }
 }
